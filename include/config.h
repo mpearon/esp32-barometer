4afe6f0f5675ae698d99dde46bcf30d526f39de8
Matthew Pearon <mpearon@users.noreply.github.com>
2024-03-27 21:45:03 -0500

    Patched config

--- include/config.h
+++ include/config.h
@@ -4,10 +4,14 @@
 #include <Arduino.h>
 #include <Stepper.h>
 #include <vector>
+#include <ota.h>
 
+extern std::string hostname;
+extern int otaPort;
 extern std::string icaoId;
 extern std::vector<double_t> gaugeFloorValues;
 extern Stepper baroStepper;
 extern int currentBaroStepperPosition;
+extern ArduinoOTAClass otaHandler;
 
 #endif
