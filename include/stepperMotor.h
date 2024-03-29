4b2c894399ac6f1243cd63c164bc03967ec45411
Matthew Pearon <mpearon@users.noreply.github.com>
2024-03-29 13:57:34 -0500

    Added stepperMotor.h  - [Based on this article](https://stackoverflow.com/questions/347358/inheriting-constructors)

---
+++ include/stepperMotor.h
@@ -0,0 +1,17 @@
+#ifndef stepperMotor_h
+#define stepperMotor_h
+
+#include <Arduino.h>
+#include <Stepper.h>
+
+class stepperMotor: public Stepper{
+	using Stepper::Stepper;
+	public:
+		std::string name;
+		int currentPosition;
+		int targetPosition;
+		int maximumStep;
+		int minimumStep;
+}
+
+#endif
